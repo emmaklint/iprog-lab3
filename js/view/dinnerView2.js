4c3cd0ca1b66702523a7bce03e2734bf59841783
Emma Klint <emma@emmaklint.com>
2016-02-25 21:50:57 +0100

    Fixed total cost while pending

--- js/view/dinnerView2.js
+++ js/view/dinnerView2.js
@@ -6,7 +6,7 @@ var DinnerView2 = function (container,model) {
 		var dishName = ('<td width="30%">' + dish.name + '</td>')
 		var dishCost = ('<td width="30%">' + cost + ' kr</td>')
 
-		$("#table").append('<tr>' + dishName + dishCost + '</tr>')
+		$("#table").append('<tr id="dish-nr-' + dish.id + '">' + dishName + dishCost + '</tr>')
 	}
 
 	this.update = function(obj) {
@@ -15,21 +15,35 @@ var DinnerView2 = function (container,model) {
 
 		this.pending = model.pendingDish();
 		pendingString = '';
+		pendingPrice = 0;
+		minusPrice = 0;
 
 		$("#table").html('<tr><th>Dishname</th><th>Cost</th></tr>');
-		$("#totalPrice").html('<p>Total: ' + model.getTotalMenuPrice() + ' kr</p>');
-
+		
 		var menu = model.getFullMenu();
 
 		for (var i = 0; i < menu.length; i++) {
 			costInfo(menu[i],model);
 		}
 
-		if(this.pending.length !== 0){
-			var pendingPrice = model.getDishPrice(this.pending[0].id);
-			pendingString += '<tr><td width="30%">Pending</td><td>' + pendingPrice + ' kr</td>'
+		if (this.pending.length !== 0){
+			for (var i = 0; i < menu.length; i++) {
+				if(this.pending[0].type == menu[i].type){
+					$("#dish-nr-" + menu[i].id).hide()
+					var removeDishPrice = true;
+					minusPrice = model.getDishPrice(menu[i].id);
+				} 
+			}
+
+			pendingPrice = model.getDishPrice(this.pending[0].id);
+			pendingString += '<tr><td width="30%">Pending</td><td width="30%">' + pendingPrice + ' kr</td>'
 			$("#table").append(pendingString);
 		}
+		
+		totMenuPrice = model.getTotalMenuPrice();
+		pendingAndTotalPrice = pendingPrice + totMenuPrice - minusPrice;
+
+		$("#totalPrice").html('<p>Total: ' + pendingAndTotalPrice + ' kr</p>');
 
 	} 
 
